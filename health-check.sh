865934f2a766dda817274dc2e1c9b7061afa1edb
pasupuletisandeep <109368411+pasupuletisandeep@users.noreply.github.com>
2023-06-02 09:39:08 +0200

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -66,7 +66,6 @@ if [[ $commit == true ]]
 then
   git config --global user.name 'Vijaye Raji'
   git config --global user.email 'vijaye@statsig.com'
-  git add -A --force logs/
   git commit -am '[Automated] Update Health Check Logs'
   git push origin main
 fi
