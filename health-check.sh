cc7a73e3fede0a107de51131be3f178d5d4a2477
pasupuletisandeep <109368411+pasupuletisandeep@users.noreply.github.com>
2023-06-02 10:19:04 +0200

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -67,4 +67,11 @@ then
   git checkout main
   git add .
   git commit -am "[Automated] Update Health Check Logs"
+  curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer <YOUR-TOKEN>"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/OWNER/REPO/statuses/SHA \
+  -d '{"state":"success","target_url":"https://example.com/build/status","description":"The build succeeded!","context":"continuous-integration/jenkins"}'
 fi
